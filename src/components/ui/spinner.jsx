25df9d2c82e6fde1049e04d68d3c274264a401b3
Abd eldjallil Meskali <djalil.meskali@gmail.com>
2026-02-11 01:43:39 +0100

    feat: add TemplateCard and Spinner components, remove AnimatedOutlet

---
+++ src/components/ui/spinner.jsx
@@ -0,0 +1,9 @@
+function Spinner() {
+  return (
+    <div className="flex h-full w-full items-center justify-center">
+      <div className="size-10 animate-spin rounded-full border-2 border-zinc-600 border-t-zinc-100" />
+    </div>
+  );
+}
+
+export { Spinner };
