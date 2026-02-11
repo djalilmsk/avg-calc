e5058a242eeb0738a25900837743aa0bebfeb8e1
Abd eldjallil Meskali <djalil.meskali@gmail.com>
2026-02-11 01:43:52 +0100

    feat: integrate CalculatorStorageProvider in App root

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,22 @@
 import Layout from "./layout";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { CalculatorStorageProvider } from "./app/calculator/context/CalculatorStorageProvider";
 
 import Home from "./app/home/page";
 import SemesterAverageApp from "./app/calculator/page";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="/calc/:template-id" element={<SemesterAverageApp />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <CalculatorStorageProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="/calc/:template-id" element={<SemesterAverageApp />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </CalculatorStorageProvider>
   );
 }
 
